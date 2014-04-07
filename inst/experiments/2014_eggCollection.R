2db41d4e75f3f39d1ef7249bd08e1360aa46d697
ValcuMihai <valcu.mihai@gmail.com>
2014-04-07 21:44:48 +0200

    -

--- inst/experiments/2014_eggCollection.R
+++ inst/experiments/2014_eggCollection.R
@@ -10,15 +10,15 @@
 	# edit(x)
 
 
-	s = function (input) 
-	{
+	s = function (input) {
 	  x = c(10, 17, 21, 26, 29, 32, 36, 38, 50, 63, 64, 67, 68, 
 	        72, 84, 85, 109, 117, 121, 122, 123, 125, 131, 137, 138, 
-	        140, 152, 161, 167, 177, 178, 179, 180, 198, 200, 214, 
-	        217, 218, 219, 235, 247, 249, 256, 261, 263, 269)
-	  sqlf = paste("SELECT distinct box,", shQuote(input$date, 
-	                                               type = "sh"), "refDate  from NESTS where box in (", paste(x, 
-	                                                                                                         collapse = ","), ")")
+	        140, 152, 161, 167, 177, 178, 179, 180, 183, 198, 200, 214, 
+	        218, 219, 225, 235, 249, 256, 261, 263, 269)
+	  
+    sqlf = paste("SELECT distinct box,", shQuote(input$date, 
+	                                               type = "sh"), "refDate  from NESTS where box in (", paste(x, collapse = ","), ")")
+    
 	  d = Q(2014, paste("select d.box, d.refDate, z.lastColDate, a.firstColDate,
 	                    DATEDIFF(d.refDate,z.lastColDate) daysSinceLastCol,
 	                    DATEDIFF(d.refDate,a.firstColDate) daysSinceFirstCol,
@@ -31,26 +31,26 @@
 	                    (select box, min(date_time) as firstColDate from NESTS where collect_eggs = 1 group by box ) a ON d.box = a.box
 	                    LEFT JOIN
 	                    (select box, max(eggs) as maxClutch from NESTS  group by box ) e ON d.box = e.box
-	                    order by firstColDate desc
+	                    order by -firstColDate desc, box
 	                    "))
-	  d$nestType = c(c("p", "p", "s"), rep(c(rep("p", 3), "s"), 11))[-47]
+	 
+    d$nestType = c( c("p", "p", "p", "p", "p", "s", "s"),  rep( c(rep("p", each = 3), "s"),  10)[-40] )
 	 
     d[is.na(d$firstColDate), "nestType"] = NA
 	  d[which(d$nestType == "p" & d$daysSinceFirstCol == 7), "col"] = "blue"
 	  
-    d[which(d$nestType == "p" & d$daysSinceFirstCol >= 8 & d$maxClutch < 8), "col"] = "blue"
-	  d[which(d$nestType == "s" & d$maxClutch %in% seq(1, 15, 
-	                                                   by = 2)), "col"] = "blue"
-	  d[which(d$nestType == "p" & d$daysSinceFirstCol >= 8 & d$maxClutch == 8), "col"] = "red"
-	  d[which(d$nestType == "p" & d$daysSinceFirstCol >= 8 & d$maxClutch == 8), "n"] = 9
-	  d[which(d$nestType == "s" & d$maxClutch %in% seq(2, 16,  by = 2)), "col"] = "red"
-	  d[which(d$nestType == "s" & d$maxClutch %in% seq(2, 16,  by = 2)), "n"] = 
-      d[which(d$nestType == "s" & d$maxClutch %in% seq(2, 16, by = 2)), "maxClutch"] + 1
+    d[which(d$nestType   == "p" & d$daysSinceFirstCol >= 8 & d$maxClutch < 8), "col"] = "blue"
+	  d[which(d$nestType   == "s" & d$maxClutch %in% seq(1, 15, by = 2)), "col"] = "blue"
+	  d[which(d$nestType   == "p" & d$daysSinceFirstCol >= 8 & d$maxClutch == 8), "col"] = "red"
+	  d[which(d$nestType   == "p" & d$daysSinceFirstCol >= 8 & d$maxClutch == 8), "n"] = 9
+	  d[which(d$nestType   == "s" & d$maxClutch %in% seq(2, 16,  by = 2)), "col"] = "red"
+	  d[which(d$nestType   == "s" & d$maxClutch %in% seq(2, 16,  by = 2)), "n"] = 
+    d[which(d$nestType   == "s" & d$maxClutch %in% seq(2, 16, by = 2)), "maxClutch"] + 1
 	  d[which(is.na(d$firstColDate)), "col"] = "red"
 	  d[which(is.na(d$col)), "col"] = "grey"
 	  d[which(is.na(d$firstColDate)), "n"] = 1
 	 
-    # require(XLConnect) ;  writeWorksheetToFile("C:/Users/valcu/Desktop/samplingEggs2014.xlsx", d, sheet='Sheet1')
+    # require(XLConnect) ;  writeWorksheetToFile("samplingEggs2014.xlsx", d, sheet='Sheet1')
     
     out = list(box = d$box, col = d$col, text = d$n)
 	 
